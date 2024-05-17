2a408e419dfa79823a347e62e4350fa7c875c9f1
VedPatel6605 <163992352+VedPatel6605@users.noreply.github.com>
2024-05-17 15:12:00 +0530

    Create 11.cpp

---
+++ 11.cpp
@@ -0,0 +1,34 @@
+// This program is created by 23IT096_VED.
+#include <iostream>
+using namespace std;
+class employee
+{
+    float exp;
+
+public:
+    employee()
+    {
+        exp = 0;
+    }
+    employee(int a, int b)
+    {
+        exp = a + (b / 12.0);
+    }
+    operator float()
+    {
+
+        return exp;
+    }
+};
+int main()
+{
+    int y, m;
+    float v;
+    cout << "Enter your experince in year" << endl;
+    cin >> y;
+    cout << "Enter your experince in month" << endl;
+    cin >> m;
+    employee e1(y, m);
+    v = e1;
+    cout << "Employee Experince is " << v << endl;
+}
